52773b5250690c5f0d221f765e6bd024fb0de278
Jenya Golovnov <93ave.blues@gmail.com>
2023-04-10 12:59:00 +0200

    added platform / replaced initial movement script

--- character.gd
+++ character.gd
@@ -1,28 +1,36 @@
 extends CharacterBody2D
 
-const SPEED = 150
-const JUMP_VELOCITY = 100
-const GRAVITY = 400
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready
-var imagePlayer = get_node("character")
+var playerImage = get_node("Sprite")
 
-func change_player_direction():
-	# Handling character's image flip
-	if velocity.x < 0:
-		imagePlayer.set_flip_h(true)
-	elif velocity.x > 0:
-		imagePlayer.set_flip_h(false)
 
-func get_user_input():
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("character_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
 	var direction = Input.get_axis("character_left", "character_right")
-	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
- 
-func _physics_process(_delta):
-	get_user_input()	
+
 	move_and_slide()
-	change_player_direction()
+	
+	# Handle sprite flip.
+	if velocity.x < 0:
+		playerImage.set_flip_h(true)
+	elif velocity.x > 0:
+		playerImage.set_flip_h(false)
+	
