19942717e5cbe9b74d7ce75d19f45d82f0e5bf37
Jenya Golovnov <93ave.blues@gmail.com>
2023-04-16 17:24:50 +0200

    new art / global variables export / added down custom action / double jump

--- character.gd
+++ character.gd
@@ -1,30 +1,38 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed: float = 200.0
+@export var jump_velocity: float = -200.0
+@export var double_jump_velocity: float = -100
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var has_double_jumped: bool = false
 
 @onready
 var playerImage = get_node("Sprite")
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Add the gravity. 
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		has_double_jumped = false
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("character_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("character_jump"):
+		if is_on_floor():
+			velocity.y = jump_velocity
+		elif not has_double_jumped:
+			velocity.y = double_jump_velocity
+			has_double_jumped = true
+			
 	var direction = Input.get_axis("character_left", "character_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
 	
